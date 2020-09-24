0e3d18557bdf3ad716d856f98fae369d4c35b953
默桓 <wenyuan.xwy@alibaba-inc.com>
2020-09-24 17:50:36 +0800

    新增一个flowsensitive case

---
+++ src/main/java/top/anemone/taintbenchmark/flowsensitive/FlowGood1.java
@@ -0,0 +1,30 @@
+package top.anemone.taintbenchmark.flowsensitive;
+
+
+import top.anemone.taintbenchmark.auxiliary.Container;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/intraprocedural/IntraBad1")
+public class FlowGood1 extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String fakeSource = request.getParameter("xss");
+        response.setContentType("text/html;");
+        String fakeClean="clean";
+        String tmp;
+        // fakeSource, fakeClean=fakeClean, fakeSource
+        tmp=fakeSource;
+        fakeSource=fakeClean;
+        fakeClean=tmp;
+
+        PrintWriter out = response.getWriter();
+        out.println(fakeSource); // get clean
+    }
+}
