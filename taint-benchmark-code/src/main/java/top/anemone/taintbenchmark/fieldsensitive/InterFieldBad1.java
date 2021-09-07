64b35a1a4272e2679956cc44adb1a94e4768eb61
Anemone95 <wenyuan.xwy@alibaba-inc.com>
2021-09-07 17:36:20 +0800

    21/09/07 add interField cases

---
+++ taint-benchmark-code/src/main/java/top/anemone/taintbenchmark/fieldsensitive/InterFieldBad1.java
@@ -0,0 +1,32 @@
+package top.anemone.taintbenchmark.fieldsensitive;
+
+
+import top.anemone.taintbenchmark.auxiliary.Container;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/FieldSensitive/FieldBad1")
+@SuppressWarnings("Duplicates")
+public class InterFieldBad1 extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String source = request.getParameter("source");
+        
+        Container<String> a = new Container<>();
+        a.setObj("clean");
+        a.setClean("clean");
+        put(a, source);
+        
+        Runtime.getRuntime().exec(a.getObj()); // sink
+    }
+    private void put(Container<String> c, String s){
+        c.setObj(s);
+    }
+}
